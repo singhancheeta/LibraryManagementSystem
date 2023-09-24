30a70984b27e0349655e85ffcb6b51742a647be5
ANCHEETA SINGH <141255357+singhancheeta@users.noreply.github.com>
2023-09-24 14:40:56 +0530

    Create Cart.js

---
+++ Cart.js
@@ -0,0 +1,15 @@
+// src/components/Cart.js
+
+import React from 'react';
+
+const Cart = () => {
+  // Implement cart functionality
+  return (
+    <div>
+      <h2>Cart</h2>
+      {/* Display cart items and total */}
+    </div>
+  );
+};
+
+export default Cart;
